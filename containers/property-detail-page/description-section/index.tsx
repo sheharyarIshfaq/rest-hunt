06be597170437d728982d9c3edc12be5498c6b3d
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-05-08 17:13:18 +0500

    Description detail modal

--- containers/property-detail-page/description-section/index.tsx
+++ containers/property-detail-page/description-section/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { BsChevronRight } from "react-icons/bs";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const PropertyDescriptionSection = () => {
   return (
@@ -12,9 +20,81 @@ const PropertyDescriptionSection = () => {
         is often conceived in three linear dimensions, although modern
         physicists usually con Physical space.
       </p>
-      <button className="flex items-center gap-2 font-semibold">
-        View full description <BsChevronRight />
-      </button>
+      <Dialog>
+        <DialogTrigger>
+          <button className="flex items-center gap-2 font-semibold">
+            View full description <BsChevronRight />
+          </button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[70vw]">
+          <DialogHeader>
+            <DialogTitle>Description</DialogTitle>
+            <DialogDescription className="pt-2 pr-2 max-h-[70vh] overflow-y-auto custom-scrollbar">
+              Physical space is often conceived in three linear dimensions,
+              although modern physicists usually con Physical space is often
+              conceived in three linear dimensions, although modern physicists
+              usually con Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space. Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space. Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.Physical space is often conceived in three linear
+              dimensions, although modern physicists usually con Physical space
+              is often conceived in three linear dimensions, although modern
+              physicists usually con Physical space is often conceived in three
+              linear dimensions, although modern physicists usually con Physical
+              space.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
