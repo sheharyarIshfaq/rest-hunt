bfaeb8511078e4a11637d4752afe886db09db842
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-05-09 00:29:00 +0500

    Added gif for confirmation

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -2,12 +2,15 @@ import Navbar from "@/components/Shared/Navbar";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import ConfirmGif from "@/public/images/confirm.gif";
+import Image from "next/image";
 
 const ConfirmationPage = () => {
   return (
     <div className="h-screen flex flex-col">
       <Navbar showSearch={false} isDark={false} />
       <div className="container flex-1 flex flex-col justify-center items-center">
+        <Image src={ConfirmGif} alt="Confirmation" className="w-36" />
         <h1 className="text-2xl font-semibold">Congratulations 🎉</h1>
         <p className="my-6 sm:max-w-[70vw] text-center">
           Your booking is confirmed, download the receipt from below. Check out
@@ -25,6 +28,7 @@ const ConfirmationPage = () => {
           </Button>
         </div>
       </div>
+      <div className="h-24"></div>
     </div>
   );
 };
