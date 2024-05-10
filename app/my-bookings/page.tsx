ddae71d443b8406c364a58595071ad3711ae53ef
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-05-10 10:43:18 +0500

    My bookings page and empty section

---
+++ app/my-bookings/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "@/components/Shared/Navbar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import NoResultSection from "@/containers/my-booking-page/no-result-section";
+import Footer from "@/components/Shared/Footer";
+
+const MyBookingsPage = () => {
+  return (
+    <>
+      <Navbar showSearch={true} />
+      <div className="container mb-8 mt-2">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <Link href="/">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>My Bookings</BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="mt-6">
+          <h1 className="text-2xl font-bold">My Bookings</h1>
+          <NoResultSection />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default MyBookingsPage;
---
+++ containers/my-booking-page/no-result-section/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NoResultImg from "@/public/images/empty.svg";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const NoResultSection = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <Image src={NoResultImg} alt="No Result" />
+      <h1 className="text-xl font-semibold mt-2">No properties booked</h1>
+      <p className="my-2 text-gray-500">
+        Find you home away from home by clicking on the button below before it
+        is too late...
+      </p>
+      <Button className="bg-main font-semibold">
+        <Link href="/search">Explore Properties</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default NoResultSection;
