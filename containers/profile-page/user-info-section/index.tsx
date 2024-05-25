30b581a2b3abc1d3f9c2601ab70c0ce222763ed9
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-05-25 22:47:04 +0500

    Updating profile

--- containers/profile-page/user-info-section/index.tsx
+++ containers/profile-page/user-info-section/index.tsx
@@ -8,12 +8,68 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useAppDispatch, useAppSelector } from "@/redux/store";
+import { toast } from "@/components/ui/use-toast";
+import { onUpdateUser } from "@/redux/features/auth-slice";
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const UserInfoSection = () => {
+  const dispatch = useAppDispatch();
+  const { user, token } = useAppSelector((state) => state.auth);
+
   const [onEditName, setOnEditName] = React.useState(false);
   const [onEditEmail, setOnEditEmail] = React.useState(false);
   const [onEditPhone, setOnEditPhone] = React.useState(false);
   const [onEditAddress, setOnEditAddress] = React.useState(false);
+
+  const [name, setName] = React.useState(user?.name || "");
+  const [email, setEmail] = React.useState(user?.email || "");
+  const [phone, setPhone] = React.useState(user?.phoneNumber || "");
+  const [address, setAddress] = React.useState(user?.location || "");
+
+  const setInitialValues = () => {
+    setName(user?.name || "");
+    setEmail(user?.email || "");
+    setPhone(user?.phoneNumber || "");
+    setAddress(user?.location || "");
+  };
+
+  const updateUserHandler = async () => {
+    try {
+      const response = await fetch(`${BACKEND_URL}/users/update`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          phoneNumber: phone,
+          location: address,
+        }),
+      });
+      const responseData = await response.json();
+      if (responseData.error) {
+        setInitialValues();
+        return toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: responseData.error,
+        });
+      }
+      const { user } = responseData;
+      dispatch(onUpdateUser(user));
+    } catch (err: any) {
+      setInitialValues();
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: err.message,
+      });
+    }
+  };
 
   return (
     <div className="my-10">
@@ -36,13 +92,19 @@ const UserInfoSection = () => {
               <div className="flex items-center gap-2 font-medium">
                 <button
                   className="text-sm"
-                  onClick={() => setOnEditName(false)}
+                  onClick={() => {
+                    setName(user?.name || "");
+                    setOnEditName(false);
+                  }}
                 >
                   Cancel
                 </button>
                 <button
                   className="text-sm text-main"
-                  onClick={() => setOnEditName(false)}
+                  onClick={() => {
+                    setOnEditName(false);
+                    updateUserHandler();
+                  }}
                 >
                   Save
                 </button>
@@ -54,6 +116,9 @@ const UserInfoSection = () => {
             placeholder="Name"
             required
             className={onEditName ? "border-main" : ""}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            readOnly={!onEditName}
           />
         </div>
         <div>
@@ -74,13 +139,19 @@ const UserInfoSection = () => {
               <div className="flex items-center gap-2 font-medium">
                 <button
                   className="text-sm"
-                  onClick={() => setOnEditEmail(false)}
+                  onClick={() => {
+                    setEmail(user?.email || "");
+                    setOnEditEmail(false);
+                  }}
                 >
                   Cancel
                 </button>
                 <button
                   className="text-sm text-main"
-                  onClick={() => setOnEditEmail(false)}
+                  onClick={() => {
+                    setOnEditEmail(false);
+                    updateUserHandler();
+                  }}
                 >
                   Save
                 </button>
@@ -92,6 +163,9 @@ const UserInfoSection = () => {
             placeholder="Email"
             required
             className={onEditEmail ? "border-main" : ""}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            readOnly={!onEditEmail}
           />
         </div>
       </div>
@@ -129,13 +203,19 @@ const UserInfoSection = () => {
               <div className="flex items-center gap-2 font-medium">
                 <button
                   className="text-sm"
-                  onClick={() => setOnEditPhone(false)}
+                  onClick={() => {
+                    setPhone(user?.phoneNumber || "");
+                    setOnEditPhone(false);
+                  }}
                 >
                   Cancel
                 </button>
                 <button
                   className="text-sm text-main"
-                  onClick={() => setOnEditPhone(false)}
+                  onClick={() => {
+                    setOnEditPhone(false);
+                    updateUserHandler();
+                  }}
                 >
                   Save
                 </button>
@@ -147,6 +227,9 @@ const UserInfoSection = () => {
             placeholder="Phone Number"
             required
             className={onEditPhone ? "border-main" : ""}
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            readOnly={!onEditPhone}
           />
         </div>
         <div>
@@ -182,13 +265,19 @@ const UserInfoSection = () => {
               <div className="flex items-center gap-2 font-medium">
                 <button
                   className="text-sm"
-                  onClick={() => setOnEditAddress(false)}
+                  onClick={() => {
+                    setAddress(user?.location || "");
+                    setOnEditAddress(false);
+                  }}
                 >
                   Cancel
                 </button>
                 <button
                   className="text-sm text-main"
-                  onClick={() => setOnEditAddress(false)}
+                  onClick={() => {
+                    setOnEditAddress(false);
+                    updateUserHandler();
+                  }}
                 >
                   Save
                 </button>
@@ -200,6 +289,9 @@ const UserInfoSection = () => {
             placeholder="Address"
             required
             className={onEditAddress ? "border-main" : ""}
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            readOnly={!onEditAddress}
           />
         </div>
       </div>
