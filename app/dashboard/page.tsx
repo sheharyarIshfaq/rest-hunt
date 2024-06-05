b492a020d70a04736732c70d2cd20984592a076d
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-06-05 07:33:59 +0500

    Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Shared/Navbar";
 import WaveIcon from "@/public/icons/wave.svg";
 import Image from "next/image";
@@ -6,20 +7,45 @@ import UserDetailsSection from "@/containers/dashboard-page/user-details-section
 import ControlsSection from "@/containers/dashboard-page/controls-section";
 import HelpSection from "@/containers/dashboard-page/help-section";
 import Footer from "@/components/Shared/Footer";
+import { useAppSelector } from "@/redux/store";
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const OwnerDashboard = () => {
+  const { user } = useAppSelector((state) => state.auth);
+  const [userData, setUserData] = useState<any>(null);
+
+  const getUserData = async () => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/${user?._id}`);
+      const data = await res.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setUserData(data?.user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    console.log(user);
+    if (user) getUserData();
+  }, [user]);
+
   return (
     <>
       <Navbar showSearch={false} />
       <div className="container mb-8 mt-2">
         <div className="flex items-center gap-2">
           <h1 className="text-2xl xs:text-3xl font-semibold">
-            Welcome, Haseeb
+            Welcome, {userData?.name || "Owner"}
           </h1>
           <Image src={WaveIcon} alt="Wave" className="w-10 xs:w-12" />
         </div>
         <div className="flex flex-col md:flex-row gap-6 md:gap-14 my-10">
-          <UserDetailsSection />
+          <UserDetailsSection user={userData} />
           <ControlsSection />
         </div>
         <HelpSection />
--- containers/dashboard-page/user-details-section/index.tsx
+++ containers/dashboard-page/user-details-section/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const UserDetailsSection = () => {
+const UserDetailsSection = ({ user }: { user: any }) => {
   return (
     <div className="flex-1">
       <div className="border-[1.5px] rounded-md p-6">
         <div className="flex items-center gap-4 border-b-[1.5px] pb-4">
           <Avatar className="w-16 h-16">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarImage src={user?.profilePicture} />
+            <AvatarFallback>
+              {user?.name
+                ?.split(" ")
+                .map((name: string) => name[0])
+                .join("")}
+            </AvatarFallback>
           </Avatar>
-          <h1 className="text-xl font-semibold">Sheharyar Ishfaq</h1>
+          <h1 className="text-xl font-semibold">{user?.name || "Owner"}</h1>
         </div>
         <div className="my-6 flex flex-col gap-4">
           <div className="flex items-center gap-2 justify-between">
@@ -18,12 +23,12 @@ const UserDetailsSection = () => {
             <p className="text-main font-medium">100%</p>
           </div>
           <div className="flex items-center gap-2 justify-between">
-            <h2>Bookings</h2>
-            <p className="text-main font-medium">100%</p>
+            <h2>Rating</h2>
+            <p className="text-main font-medium">{user?.averageRating}/5</p>
           </div>
           <div className="flex items-center gap-2 justify-between">
-            <h2>Rating</h2>
-            <p className="text-main font-medium">4.6</p>
+            <h2>Reviews Count</h2>
+            <p className="text-main font-medium">{user?.reviewsCount}</p>
           </div>
         </div>
       </div>
