3c7b28ed41482928f5ed84e0b6850ac2512fc561
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-06-06 10:26:07 +0500

    Fixed height for slider images

--- containers/property-detail-page/image-section/index.tsx
+++ containers/property-detail-page/image-section/index.tsx
@@ -17,7 +17,7 @@ import Image from "next/image";
 
 const ImageSlide = ({ image }: any) => {
   return (
-    <div className="rounded-lg overflow-hidden max-h-[450px]">
+    <div className="rounded-lg overflow-hidden h-[350px]">
       <Image
         src={image}
         alt="property"
